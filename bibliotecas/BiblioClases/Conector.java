2004fe2b25580f1e27aa3d2ac90a6dfdd44d6e71
Jagoba Peruarena Martinez <Jagoba Peruarena Martinez@HZ114822.H012108.NET>
2023-01-31 08:31:00 +0100

    primera pagina

--- bibliotecas/BiblioClases/Conector.java
+++ bibliotecas/BiblioClases/Conector.java
@@ -1,5 +1,28 @@
 package BiblioClases;
 
-public class Conector {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Conector {
+	Connection con;
+	public void Conectar() throws ClassNotFoundException, SQLException {
+		final String HOST="jdbc:mysql://localhost/";
+		final String BBDD="biblioteca";
+		final String USERNAME="root";
+		final String PASSWORD="";
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		con = DriverManager.getConnection(HOST+BBDD,USERNAME,PASSWORD);
+	
+	}
+	public void cerrar() throws SQLException {
+		con.close();
+	}
+	public Connection getCon() {
+		return con;
+	}
+	public void setCon(Connection con) {
+		this.con = con;
+	}
+	
 }
---
+++ bibliotecas/BiblioClases/Menu.java
@@ -0,0 +1,27 @@
+package BiblioClases;
+
+public class Menu {
+	public static void monstrarMenuPrincipal() {
+		System.out.println("Menu Principal:");
+		System.out.println("1. Gestionar Libros");
+		System.out.println("2. Gestionar Socios");
+		System.out.println("3. Gestionar Prestamos");
+		
+	}
+	public static void mostrarMenuLibros() {
+		System.out.println("Menu Libros:");
+		System.out.println("1. Insertar Libro");
+		System.out.println("2. Eliminar Libro");
+		System.out.println("3. Ver Libros");
+		
+	}
+	public static void mostrarMenuSocios() {
+		System.out.println("Menu Socios:");
+		System.out.println("1. Ver Socios");
+	}
+	public static void mostrarMenuPrestamos() {
+		System.out.println("Menu Prestamos:");
+		System.out.println("1. Realizar préstamo");
+	}
+
+}
