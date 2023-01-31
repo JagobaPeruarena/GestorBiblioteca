658580dba42019e5e65ee290ce88296a1839cfc8
Jagoba Peruarena Martinez <Jagoba Peruarena Martinez@HZ114822.H012108.NET>
2023-01-31 10:01:21 +0100

    quinto commit

--- bibliotecas/BiblioClases/GestorLibros.java
+++ bibliotecas/BiblioClases/GestorLibros.java
@@ -1,22 +1,31 @@
 package BiblioClases;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class GestorLibros {
-	public static void run(Scanner scan) {
+	public static void run(Scanner scan) throws ClassNotFoundException, SQLException {
 		int opcion=1;
+		GestorBBDD gesto = new GestorBBDD();
 		do {
 			Menu.monstrarMenuPrincipal();
 			opcion = Integer.parseInt(scan.nextLine());
 			switch (opcion) {
 			case Menu.INSERTAR_LIBROS:
 				System.out.println("Insertar Libro");
+				gesto.Conectar();
+				gesto.insertarLibro(null);
+				gesto.cerrar();
 				break;
 			case Menu.ELIMINAR_LIBROS:
 				System.out.println("Eliminar Libro");
+				gesto.Conectar();
+				gesto.eliminarLibro(0);
+				gesto.cerrar();
 				break;
 			case Menu.VER_LIBROS:
 				System.out.println("Ver Libro");
+				gesto.getLibro(0);
 				break;
 			case Menu.SALIR:
 				System.out.println("Saliendo");
--- bibliotecas/BiblioClases/Visor.java
+++ bibliotecas/BiblioClases/Visor.java
@@ -1,5 +1,21 @@
 package BiblioClases;
 
+import java.util.ArrayList;
+
+
 public class Visor {
+	public static void mostrarLibros(ArrayList<Libro> libros) {
+		for (Libro libro: libros) {
+			libro.toString();
+		}
+	}
+	public static void mostrarLibro(Libro libro) {
+		libro.toString();
+		
+	}
+	public static void mostrarMensaje(String mensaje) {
+		System.out.println(mensaje);
+	}
+	
 
 }
